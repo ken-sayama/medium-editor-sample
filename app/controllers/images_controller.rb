8e60c4dddafb40fd49f0e634df697d4669ef8ad2
ken-sayama <sayamaken0402@gmail.com>
2018-04-17 12:55:00 +0900

    fix upload function

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -61,6 +61,38 @@ class ImagesController < ApplicationController
     end
   end
 
+  def upload
+    files = params.require(:files)
+
+    @image = Image.new
+    @image.file = files[0]
+
+    respond_to do |format|
+      if @image.save
+        format.html { redirect_to @image, notice: 'Image was successfully created.' }
+        format.json do
+          render json: {
+            files:
+              [
+                {
+                  url: @image.file.url,
+                  thumbnail_url: @image.file.url,
+                  name: @image.file_identifier,
+                  type: "image/jpeg",
+                  size: 0,
+                  delete_url: "your_delete_url",
+                  delete_type: "DELETE"
+                }
+              ]
+          }
+        end
+      else
+        format.html { render :new }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_image
